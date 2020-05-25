2e3363823bbf8eadb603391cdc0a27f320964924
vogelweide1985 <vogelweide1985@hotmail.de>
2020-05-25 16:37:12 +0200

    added metatag

--- retrieve_data.R
+++ retrieve_data.R
@@ -6,11 +6,16 @@ library(dplyr)
 country <- c('DE') #set the geographic area: DE = Germany
 time <- ("2015-01-01 2020-05-24") #set the time window
 channel <- 'web' #set channels
-hl <- "de"
-keywords <- c("ostsee urlaub", "nordsee urlaub", "schwarzwald urlaub", "allgäu urlaub",
-              "aida urlaub", "ägypten urlaub" ) #define the keywords
+hl <- "de" #set lanaguage
 
+tag <- "Urlaub"
+keywords <- c("ostsee", "nordsee",
+              "schwarzwald", "allgäu",
+              "aida","kreuzfahrt",
+              "spanien","italien", "griechenland",
+              "tunesien","ägypten", "türkei" ) #define the keywords
 
+keywords <- paste(keywords, tag, sep= " ")
 
 
 trends <- list()
@@ -19,6 +24,15 @@ for (i in 1: length(keywords)) {
 }
 
 trends <-  bind_rows(trends)
+
+#Meta Categorien erstellen
+trends$region <- ifelse(trends$keyword %in% keywords[1:2], "Küste","" )
+trends$region <- ifelse(trends$keyword %in% keywords[3:4], "Berge",trends$region )
+trends$region <- ifelse(trends$keyword %in% keywords[5:6], "Kreuzfahrt",trends$region )
+trends$region <- ifelse(trends$keyword %in% keywords[7:9], "Nord-Mittelmeer",trends$region )
+trends$region <- ifelse(trends$keyword %in% keywords[10:12], "Süd-Mittelmeer",trends$region )
+trends$region <- as.factor(trends$region)
+
 
 #saveRDS(trends <- list(trends_all = trends_all, trends_solo = trends_solo), "trends.RDS")
 saveRDS(trends, "trends.RDS")
